255334bdeec6bef43d8ec207e63f362bc66fd36d
kdabi <kritagya577@gmail.com>
2017-10-23 22:59:53 +0530

    added unet generator class

--- pix2pix.py
+++ pix2pix.py
@@ -8,6 +8,19 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import os
 
+if torch.cuda.is_available():
+    use_gpu = True
+
+def init_weights(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.uniform(m.weight.data, 0.0, 0.02)
+    elif classname.find('Linear') != -1:
+        init.uniform(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:
+        init.uniform(m.weight.data, 1.0, 0.02)
+        init.constant(m.bias.data, 0.0)
+
 # from .base_model import BaseModel
 class Pix2Pix(nn.Module):
 	def __init__(self, opt):
@@ -17,9 +30,11 @@ class Pix2Pix(nn.Module):
 		self.input_A = self.Tensor()
 		self.input_B = self.Tensor()
 
-
-
-
+                norm_layer = get_norm_layer(norm_type=norm)
+                self.GeneraterNet = Generator(input_nc, output_nc, 8, ngf, norm_layer=norm_layer, use_dropout= use_dropout)
+                if use_gpu:
+                    self.GeneraterNet.cuda()
+                self.GeneraterNet.apply(init_weights)
 
 
 
@@ -33,12 +48,73 @@ class Pix2Pix(nn.Module):
 		save_path = "./saved_models/%s_net_%s.pth" % (epoch_label, network_label)
 		# torch.save(network.cpu().state_dict(), save_path)
 		network.load_state_dict(torch.load(save_path))
-		
+
 	def update_learning_rate(self):
 		pass
 
-class UnetBlockGenerater(nn.Module):
-	def __init__(self, outer_nc, inner_nc, )
+class Generator(nn.Module):
+    def __init__(self, input_nc, output_nc, num_downs, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False)
+        super(UnetGenerator, self).__init__()
+
+        # constructing the unet generator structure
+        generator_block = UnetBlock(ngf*8, ngf*8, input_nc=None, submodule=None, norm_layer=norm_layer, innermost=True)
+        for i in range(num_down -5):
+            generator_block = UnetBlock(ngf*8, ngf*8, input_nc=None, submodule=generator_block, norm_layer=norm_layer, use_drop=use_dropout)
+
+        generator_block = UnetBlock(ngf*4, ngf*8, input_nc=None,submodule=generator_block, norm_layer=norm_layer)
+        generator_block = UnetBlock(ngf*2, ngf*4, input_nc=None,submodule=generator_block, norm_layer=norm_layer)
+        generator_block = UnetBlock(ngf, ngf*2, input_nc=None,submodule=generator_block, norm_layer=norm_layer)
+        generator_block = UnetBlock(output_nc, ngf, input_nc=input_nc,submodule=generator_block, outermost=True, norm_layer=norm_layer)
+
+        self.model = generator_block
+
+    def forward(self, input):
+        return self.model(input)
 
 
-	
+class UnetBlock(nn.Module):
+    def __init__(self, outer_nc, inner_nc, input_nc = None, submodule = None, outermost=False,innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False):
+        super(UnetBlock, self).__init__()
+        self.outermost = outermost
+
+        if input_nc is None:
+            input_nc = outer_nc
+
+
+        downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4, stride=2, padding=1)
+        downrelu = nn.LeakyReLU(0,2, True)
+        downnorm = norm_layer(inner_nc)
+        uprelu = nn.ReLU(True)
+        upnorm = norm_layer(outer_nc)
+
+        if outermost:
+            upconv = nn.ConvTranspose2d(inner_nc*2, outer_nc, kernel_size=4, stride=2, padding=1)
+            down = [downconv]
+            up = [uprelu, upconv, nn.Tanh()]
+            model = down + [submodule] + up
+        elif innermost:
+            upconv = nn.ConvTranspose2d(inner_nc, outer_nc, kernel_size=4, stride=2, padding=1)
+            down = [downrelu, downconv]
+            up = [uprelu, upconv, upnorm]
+            model = down + up
+        else:
+            upconv = nn.ConvTranspose2d(inner_nc*2, outer_nc, kernel_size=4, stride=2, padding=1)
+            down = [downrelu, downconv, downnorm]
+            up = [uprelu, upconv, upnorm]
+
+            if use_dropout:
+                model = down+[submodule] + up + [nn.Dropout(0.5)]
+            else:
+                model = down + [submodule] + up
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        if self.outermost:
+            return self.model(x)
+        return
+            return torch.cat([x, self.model(x)], 1)
+
+
+
+
