db6581aa113b8dca2005df1ee830163473635d28
kdabi <kritagya577@gmail.com>
2017-10-28 15:47:41 +0530

    added vgg code

--- Dataset_Generator/changefileName.py
+++ Dataset_Generator/changefileName.py
@@ -1,4 +1,5 @@
 import os,sys
+import numpy as np
 import cv2
 path = sys.argv[1]
 files = os.listdir(path)
@@ -8,6 +9,7 @@ for File in  files:
     image = cv2.imread(os.path.join(path, File))
     os.remove(os.path.join(path, File))
     resized_image = cv2.resize(image, (256, 256))
-    cv2.imwrite(os.path.join(path,str(i)+'.png'), resized_image)
+    #resized_image = np.concatenate((resized_image, resized_image), axis=1)
+    cv2.imwrite(os.path.join(path,str(i)+'.jpg'), resized_image)
     i=i+1
 
--- Dataset_Generator/edge_detection.py
+++ Dataset_Generator/edge_detection.py
@@ -6,8 +6,13 @@ import os
 '''
 # read command-line filename argument
 path = sys.argv[1]
-output_path = sys.argv[2]
-
+path_train = './train'
+path_test = './test'
+path_val = './val'
+i=0
+valI =0
+valTrain =0
+valTest =0
 for filename in os.listdir(path):
     # load original image as grayscale
     if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith("._jpeg_"):
@@ -16,6 +21,20 @@ for filename in os.listdir(path):
 
         # set up display window with trackbars for minimum and maximum threshold
         # values
+        i = i+1
+        if i < 401:
+            output_path = path_train
+            valTrain = valTrain +1
+            #newfilename = filename
+            newfilename = str(valTrain)+'.jpg'
+        elif i < 501:
+            valI = valI +1
+            newfilename = str(valI)+'.jpg'
+            output_path = path_val
+        else:
+            valTest = valTest +1
+            newfilename = str(valTest)+'.jpg'
+            output_path = path_test
         image_original = cv2.imread(os.path.join(path, filename))
         minT = 50
         maxT = 90
